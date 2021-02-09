c11194a6e85ff9cf271cf99ecc32244c1c731a6b
Avi Patel <avipatel805>
2021-02-08 22:00:44 -0600

    Base game
    2 players
    no AI
    GUI
    pycharm ready

---
+++ venv/endgame.py
@@ -0,0 +1,27 @@
+def boardFull(bo):
+    count = 0
+    for item in bo:
+        if item != ' ':
+            count += 1
+    if count >= 9:
+        return True
+    return False
+
+def Winner(bo, let):
+    if bo[1] == let and bo[2] == let and bo[3] == let:
+        return True
+    if bo[4] == let and bo[5] == let and bo[6] == let:
+        return True
+    if bo[7] == let and bo[8] == let and bo[9] == let:
+        return True
+    if bo[1] == let and bo[4] == let and bo[7] == let:
+        return True
+    if bo[2] == let and bo[5] == let and bo[8] == let:
+        return True
+    if bo[3] == let and bo[6] == let and bo[9] == let:
+        return True
+    if bo[1] == let and bo[5] == let and bo[9] == let:
+        return True
+    if bo[3] == let and bo[5] == let and bo[7] == let:
+        return True
+    return False
